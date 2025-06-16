bdf135d6cca7a1f966254c1fcf8a2486bcde589a
Vindya2004 <vindyamadubhashini@gmail.com>
2025-06-16 13:51:06 +0530

    add DBCP

--- src/main/java/org/example/complaint_management_system_jsp/db/DBCP.java
+++ src/main/java/org/example/complaint_management_system_jsp/db/DBCP.java
@@ -1,4 +1,33 @@
 package org.example.complaint_management_system_jsp.db;
 
-public class DBCP {
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+@WebListener
+public class DBCP implements ServletContextListener {
+    @Override
+   public void contextInitialized(ServletContextEvent sce) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cms");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Ijse@1234");
+        dataSource.setInitialSize(50);
+        dataSource.setMaxTotal(100);
+
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("ds", dataSource);
+
+    }
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            BasicDataSource ds = (BasicDataSource) sce.getServletContext().getAttribute("dataSource");
+            ds.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
